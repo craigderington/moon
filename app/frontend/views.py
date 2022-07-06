a5263956f7c024fd2f61a8f7bc69e5c2f2d48743
Craig Derington <craigderington@protonmail.com>
2022-07-05 19:39:20 -0700

    Update frontend views and templates.

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,9 +1,45 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, render_template, url_for
+from datetime import datetime
+
 
 # create frontend blueprint
-frontend = Blueprint("frontend", __name__)
+frontend = Blueprint("frontend", __name__, template_folder="templates")
 
 
 @frontend.route("/", methods=["GET"])
 def default_route():
     return redirect(url_for("faucet.index"))
+
+
+@frontend.route("/about", methods=["GET"])
+def about():
+    context = {}
+    return render_template(
+        "frontend/about.html",
+        context=context,
+        today=get_date()
+    )
+
+
+@frontend.route("/donate", methods=["GET"])
+def donate():
+    context = {}
+    return render_template(
+        "frontend/donate.html",
+        context=context,
+        today=get_date()
+    )
+
+
+@frontend.route("/help", methods=["GET"])
+def help():
+    context = {}
+    return render_template(
+        "frontend/help.html",
+        context=context,
+        today=get_date()
+    )
+
+
+def get_date():
+    return datetime.now().strftime("%c")
